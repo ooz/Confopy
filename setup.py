85ea75129d644df46d522677a09f2f1dc2259268
ooz <oliverzscheyge@gmail.com>
2014-09-20 11:43:17 +0200

    Set numpy dep to v1.6.2

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ except ImportError:
 
 setup(
     name="Confopy",
-    version="0.3.7",
+    version="0.3.8",
     url="https://github.com/ooz/Confopy",
     author="Oliver Zscheyge",
     author_email="oliverzscheyge@gmail.com",
@@ -19,7 +19,7 @@ setup(
     data_files = ["README.md"],
     install_requires=[
         "lxml >= 3.3.5",
-        "numpy >= 1.6.2",
+        "numpy == 1.6.2",
         "nltk >= 3.0.0",
         "Pattern == 2.6",
         "pyenchant == 1.6.5",
