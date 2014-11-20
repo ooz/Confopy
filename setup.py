dbb746412b9b5551db41ff3b9ac1bb906891ae1d
ooz <oliverzscheyge@gmail.com>
2014-11-20 18:04:13 +0100

    v0.4.5

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ except ImportError:
 
 setup(
     name="Confopy",
-    version="0.4.4",
+    version="0.4.5",
     url="https://github.com/ooz/Confopy",
     author="Oliver Zscheyge",
     author_email="oliverzscheyge@gmail.com",
